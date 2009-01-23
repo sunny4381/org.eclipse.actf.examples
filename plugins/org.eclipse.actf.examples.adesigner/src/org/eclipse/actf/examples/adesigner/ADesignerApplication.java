1112f1aad111afc48cd99d9ffaef7d6c132a833f
kfukuda <kfukuda>
2009-01-23 09:33:44 +0000

    [245847] reduce warnings

--- plugins/org.eclipse.actf.examples.adesigner/src/org/eclipse/actf/examples/adesigner/ADesignerApplication.java
+++ plugins/org.eclipse.actf.examples.adesigner/src/org/eclipse/actf/examples/adesigner/ADesignerApplication.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2008 IBM Corporation and Others
+ * Copyright (c) 2006, 2009 IBM Corporation and Others
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,26 +10,50 @@
  *******************************************************************************/
 package org.eclipse.actf.examples.adesigner;
 
-import org.eclipse.core.runtime.IPlatformRunnable;
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
 
+public class ADesignerApplication implements IApplication {
 
-
-public class ADesignerApplication implements IPlatformRunnable {
-
-	public Object run(Object args) throws Exception {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.equinox.app.IApplication#start(org.eclipse.equinox.app.IApplicationContext)
+	 */
+	public Object start(IApplicationContext context) {
 
 		Display display = PlatformUI.createDisplay();
 
 		try {
-			int returnCode = PlatformUI.createAndRunWorkbench(display, new ApplicationWorkbenchAdvisor());
+			int returnCode = PlatformUI.createAndRunWorkbench(display,
+					new ApplicationWorkbenchAdvisor());
 			if (returnCode == PlatformUI.RETURN_RESTART) {
-				return IPlatformRunnable.EXIT_RESTART;
+				return IApplication.EXIT_RESTART;
 			}
-			return IPlatformRunnable.EXIT_OK;
+			return IApplication.EXIT_OK;
 		} finally {
 			display.dispose();
 		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.equinox.app.IApplication#stop()
+	 */
+	public void stop() {
+		final IWorkbench workbench = PlatformUI.getWorkbench();
+		if (workbench == null)
+			return;
+		final Display display = workbench.getDisplay();
+		display.syncExec(new Runnable() {
+			public void run() {
+				if (!display.isDisposed())
+					workbench.close();
+			}
+		});
 	}
 }
