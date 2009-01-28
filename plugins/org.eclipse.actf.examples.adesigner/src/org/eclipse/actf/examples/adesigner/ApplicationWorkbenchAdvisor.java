b247652451172483750a09603cc4a0b8217e56ae
kfukuda <kfukuda>
2009-01-28 13:04:21 +0000

    [260231] integration into Eclipse platform

--- plugins/org.eclipse.actf.examples.adesigner/src/org/eclipse/actf/examples/adesigner/ApplicationWorkbenchAdvisor.java
+++ plugins/org.eclipse.actf.examples.adesigner/src/org/eclipse/actf/examples/adesigner/ApplicationWorkbenchAdvisor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2008 IBM Corporation and Others
+ * Copyright (c) 2006, 2009 IBM Corporation and Others
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *    Norimasa HAYASHIDA - initial API and implementation
+ *    Kentarou FUKUDA - initial API and implementation
  *******************************************************************************/
 package org.eclipse.actf.examples.adesigner;
 
@@ -25,18 +26,19 @@ public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 	}
 
 	public String getInitialWindowPerspectiveId() {
+		String pId = null;
 		if (ADesignerPlugin.getPerspectiveID() == null) {
-			return ADesignerPlugin.getDefault().getPluginPreferences()
+			pId = ADesignerPlugin.getDefault().getPluginPreferences()
 					.getString(
 							IPreferenceConstants.SELECTED_MODE_PERSPECTIVE_ID);
 		} else {
-			String pID = ADesignerPlugin.getPerspectiveID();
-			if (PlatformUI.getWorkbench().getPerspectiveRegistry()
-					.findPerspectiveWithId(pID) != null) {
-				return pID;
-			}else{
-				return IVisualizationPerspective.ID_HTML_PERSPECTIVE;
-			}
+			pId = ADesignerPlugin.getPerspectiveID();
+		}
+		if (PlatformUI.getWorkbench().getPerspectiveRegistry()
+				.findPerspectiveWithId(pId) != null) {
+			return pId;
+		} else {
+			return IVisualizationPerspective.ID_HTML_PERSPECTIVE;
 		}
 	}
 
