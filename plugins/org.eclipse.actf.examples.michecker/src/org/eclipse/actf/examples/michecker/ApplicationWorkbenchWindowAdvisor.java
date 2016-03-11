41a9a73fc3b9498fad2086577b8a04d380ac5fe5
Kentarou Fukuda <kentarou@jp.ibm.com>
2016-03-11 11:48:34 +0900

    Bug 489097 - documentation for HTML5 support

--- plugins/org.eclipse.actf.examples.michecker/src/org/eclipse/actf/examples/michecker/ApplicationWorkbenchWindowAdvisor.java
+++ plugins/org.eclipse.actf.examples.michecker/src/org/eclipse/actf/examples/michecker/ApplicationWorkbenchWindowAdvisor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2011 Ministry of Internal Affairs and Communications (MIC),
+ * Copyright (c) 2010, 2016 Ministry of Internal Affairs and Communications (MIC),
  * IBM Corporation and Others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -17,8 +17,6 @@ import org.eclipse.actf.ui.util.PlatformUIUtil;
 import org.eclipse.actf.visualization.eval.guideline.GuidelineHolder;
 import org.eclipse.actf.visualization.eval.guideline.IGuidelineData;
 import org.eclipse.actf.visualization.ui.IVisualizationView;
-import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
-import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.jface.action.IContributionItem;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.preference.IPreferenceNode;
@@ -28,13 +26,11 @@ import org.eclipse.ui.IPerspectiveDescriptor;
 import org.eclipse.ui.IPerspectiveListener;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchPreferenceConstants;
-import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
-import org.eclipse.ui.internal.WorkbenchWindow;
 
 public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 
@@ -83,7 +79,7 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 						.getWorkbench()
 						.getHelpSystem()
 						.resolve(
-								"/org.eclipse.actf.examples.michecker.doc/docs/overview.html#guide_2_2", //$NON-NLS-1$
+								"/org.eclipse.actf.examples.michecker.doc/docs/readme1st.html", //$NON-NLS-1$
 								true));
 
 	}
@@ -103,6 +99,7 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 		}
 
 		// hide quick access (for Eclipse 4.2.x)
+		/*
 		try {
 			IWorkbenchWindow window = PlatformUI.getWorkbench()
 					.getActiveWorkbenchWindow();
@@ -113,6 +110,7 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 		} catch (Exception e) {
 
 		}
+		*/
 
 		PreferenceManager prefManager = getWindowConfigurer()
 				.getWorkbenchConfigurer().getWorkbench().getPreferenceManager();
